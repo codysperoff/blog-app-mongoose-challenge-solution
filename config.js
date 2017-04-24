6c240708a82884b5f30fa262deaf6948295137a9
Cody Speroff <cnsperoff@gmail.com>
2017-04-24 09:35:07 -0500

    updated database_url in config file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+    'mongodb: //admin:admin@ds115671.mlab.com:15671/node-blog-app-mongoose-tests';
 exports.PORT = process.env.PORT || 8080;
